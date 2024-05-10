7bb83348abe73f5e514f7d1c9979457a5b746e92
灿烂 <1481086138@qq.com>
2024-05-10 14:15:57 +0800

    完成ColumnList组件

---
+++ src/components/ColumnList.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="row">
+    <div v-for="item in columnList" :key="item.id" class="col-4 mb-4">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img :src="item.avatar" :alt="item.title" class="rounded-circle border border-light w-25 my-3">
+          <h5 class="card-title">{{ item.title }}</h5>
+          <p class="card-text text-left">{{ item.description }}</p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import emptyPic from '@/assets/imgs/column.jpg'
+import { computed, defineComponent, type PropType } from 'vue'
+
+
+export interface ColumnProps {
+  id: number
+  title: string
+  avatar?: string
+  description: string
+}
+
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    list: {
+      // 将构造函数断言成类型，需要使用PropType
+      type: Array as PropType<ColumnProps[]>,
+      required: true
+    }
+  },
+  setup(props) {
+    const columnList = computed(() => {
+      return props.list.map((item) => {
+        if(!item.avatar) {
+          item.avatar = emptyPic
+        }
+        return item
+      })
+    })
+    return {
+      columnList,
+    }
+  }
+})
+</script>
+<style lang=""> 
+
+</style>
---
+++ src/components/template.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Demo</div>
+</template>
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Demo',
+  setup() {
+    
+  }
+})
+</script>
+<style scoped>
+  
+</style>
