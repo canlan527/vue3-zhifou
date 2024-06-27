b6eb3f5e37d2f4f934ef0ccb61e2bdddbfed73f2
灿烂 <1481086138@qq.com>
2024-06-27 23:48:43 +0800

    loader组件

---
+++ src/components/Loader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="loader-container d-flex justify-content-center align-items-center h-100">
+    <div class="loading-content">
+      <div class="spinner-border text-primary" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Loader',
+  setup() {
+    
+  }
+})
+</script>
+<style scoped>
+  
+</style>
