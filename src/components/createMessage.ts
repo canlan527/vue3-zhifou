fa8e7489ca3fe2261c9c39a0acc66891f7d9dd7c
灿烂 <1481086138@qq.com>
2024-07-24 00:24:38 +0800

    使用h函数优化createMessage

--- src/components/createMessage.ts
+++ src/components/createMessage.ts
@@ -1,23 +1,34 @@
-import { createApp } from "vue";
+import { createApp, h, render } from "vue";
 import Message from "./Message.vue";
 
 export type MessageType = "success"  | "default" | "error";
 
-const createMessage = (type: MessageType, message: string,  timeout: number = 1500) => {
+const createMessage = (type: MessageType, message: string,  timeout?: number) => {
   // 创建实例
-  const messageInstance = createApp(Message, {
+  const messageVNode = h(Message, {
     message,
     type,
   })
   // 挂载到节点
   const mountNode = document.createElement("div");
   document.body.appendChild(mountNode);
-  messageInstance.mount(mountNode);
-  // 定时销毁
-  setTimeout(() => {
-    messageInstance.unmount();
+  render(messageVNode, mountNode);
+
+  // 销毁
+  const destory = () => {
+    render(null, mountNode);
     document.body.removeChild(mountNode);
-  }, timeout);  
+  }
+  // 定时销毁
+  if(timeout) {
+    setTimeout(() => {
+      destory();
+    }, timeout);  
+  }
+
+  return {
+    destory,
+  }
 }
 
 export default createMessage; 
